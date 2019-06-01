5c81b253ebdc3d056777168f110744f11957e276
SuJunJie <864273312@qq.com>
2019-06-01 19:52:02 +0800

    完成所有网页公有样式结构的搭建、动画编写、退出登录功能、拦截登录功能

--- public/back/js/common.js
+++ public/back/js/common.js
@@ -5,7 +5,7 @@
 $(function(){
 
 
-//功能1、进度条开启和关闭
+    //全局功能、进度条开启和关闭
     progressBar();
     function progressBar(){
         //进度条开启
@@ -21,6 +21,77 @@ $(function(){
             },500);
         });
     }
+
+
+
+    //全局功能、登录拦截功能
+    if ( location.href.indexOf("login.html") === -1 ) {
+        // 地址栏中没有 login.html, 说明不是登录页, 需要进行登录拦截
+        $.ajax({
+            type:'get',
+            url:'/employee/checkRootLogin',
+            dataType:'json',
+            success:function( info ){
+                if( info.success ){
+                    //用户已登录 什么都不做
+                }
+
+                if(info.error === 400){
+                    //用户未登录 拦截到登录页
+                    location.href = "login.html";
+                }
+            }
+        });
+
+
+    }
+
+
+
+    //功能1：点击分类管理 二级分类列表DropDown+过渡
+
+    $("#categroy").on("click",function(){
+
+        $(".secondNav").stop().slideToggle();
+    });
+
+
+
+
+    //功能2：点击菜单按钮 隐藏侧边栏
+
+    $(".lt-content .topbar .menu").on("click",function(){
+
+        $(".lt-aside").toggleClass("hide-aside");
+        $(".lt-content").toggleClass("hide-aside");
+        $(".lt-content .topbar").toggleClass("hide-aside");
+
+    });
+
+
+
+
+    //功能3：退出功能
+    $("#btn-logout").on("click",function(){
+
+        $.ajax({
+            type:'get',
+            url:'/employee/employeeLogout',
+            dataType:'json',
+            success:function( info ){
+                //console.log("请求成功");
+                //console.log(info);
+
+                if( info.success ){
+                    location.href = "login.html";
+                }
+
+
+            }
+
+        });
+
+    });
 
 
 
