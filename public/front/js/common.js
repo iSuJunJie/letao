6b065f8031d52924c62b5119beda1290bdce3620
SuJunJie <864273312@qq.com>
2019-06-04 09:55:22 +0800

    公共模块及样式完成

---
+++ public/front/js/common.js
@@ -0,0 +1,100 @@
+/**
+ * Created by Campbell on 2019/6/3.
+ */
+//入口函数
+$(function(){
+
+    //功能1：初始化区域滚动模块
+    mui('.mui-scroll-wrapper').scroll({
+        scrollY: true, //是否竖向滚动
+        scrollX: false, //是否横向滚动
+        startX: 0, //初始化时滚动至x
+        startY: 0, //初始化时滚动至y
+        indicators: true, //是否显示滚动条
+        deceleration:0.0006, //阻尼系数,系数越小滑动越灵敏
+        bounce: true //是否启用回弹
+    });
+
+
+
+    //功能2：手动初始化图片轮播模块
+    //获得slider插件对象
+    var gallery = mui('.mui-slider');
+    gallery.slider({
+        interval:1000//自动轮播周期，若为0则不自动播放，默认为0；
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
